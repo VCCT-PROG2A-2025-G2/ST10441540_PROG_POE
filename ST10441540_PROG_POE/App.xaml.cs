9baeb3cd5859f6f90d372605524265b2c566e262
brndnless@gmail.com <brndnless@gmail.com>
2025-06-27 18:02:49 +0200

    Add project files.

---
+++ ST10441540_PROG_POE/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ST10441540_PROG_POE;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
